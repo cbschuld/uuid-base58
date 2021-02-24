29ee0e8a9d8c0da2970eb08938a45b1ed25c5ef3
Chris Schuld <chris@chrisschuld.com>
2021-02-23 18:24:11 -0700

    ignore empty for linter

--- src/uuid58.ts
+++ src/uuid58.ts
@@ -37,6 +37,7 @@ export const valid = (uuidBase58: string): boolean => {
   try {
     return decode(uuidBase58).length === 36;
   }
+  // tslint:disable-next-line:no-empty
   catch(e) {}
   return false;
 }
