203781982320c606883fa25227f38f2a2ee48c03
Chris Schuld <cbschuld@gmail.com>
2022-07-05 06:49:37 -0700

    added ESM modules; added workflow for github and release for npm

--- __tests__/v4.test.ts
+++ __tests__/v4.test.ts
@@ -1,5 +1,12 @@
 import { v4 as uuid } from '../src/uuid/v4';
 
-test('v4 calculation', () => {
-    expect(uuid().length).toBeGreaterThan(20);
-});
+describe('v4 testing', ()=> {
+    test('calculation', () => {
+        expect(uuid().length).toBeGreaterThan(20);
+    });   
+    test('simplistic unique check', () => {
+        const uuid1 = uuid();
+        const uuid2 = uuid();
+        expect(uuid1).not.toBe(uuid2);
+    });
+})
