5b91d744898b187443aebf99fd75bbea2cf37bc1
Chris Schuld <cbschuld@gmail.com>
2022-12-30 11:53:04 -0700

    updates to expose uuidV4NoDash

--- __tests__/v4.test.ts
+++ __tests__/v4.test.ts
@@ -1,4 +1,5 @@
 import { v4 as uuid } from '../src/uuid/v4';
+import { uuidV4NoDash } from '../src/index';
 
 describe('v4 testing', ()=> {
     test('calculation', () => {
@@ -7,6 +8,17 @@ describe('v4 testing', ()=> {
     test('simplistic unique check', () => {
         const uuid1 = uuid();
         const uuid2 = uuid();
+        expect(uuid1).not.toBe(uuid2);
+    });
+})
+
+describe('v4 testing without Dashes', ()=> {
+    test('calculation', () => {
+        expect(uuidV4NoDash().length).toBeGreaterThan(20);
+    });   
+    test('simplistic unique check', () => {
+        const uuid1 = uuidV4NoDash();
+        const uuid2 = uuidV4NoDash();
         expect(uuid1).not.toBe(uuid2);
     });
 })
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export const uuid58 = () : string => encode(uuidNoDash());
 export const encode = (id:string) => base58encode(id);
 export const decode = (id:string) => base58decode(id);
 export const valid = (id:string) => base58valid(id);
+export const uuidV4NoDash = () => uuidNoDash();
